c1a30c2ce5ba87f45eb6eb111c086f2ff0b0ec6a
German <gexxx@bk.ru>
2023-11-29 17:27:07 +0300

    new

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,19 @@
-import React, { Component } from "react";
-
+import React, { Component, Fragment } from "react";
 import "../styles/App.css";
+
+import Header from "./header";
 
 class App extends Component {
     render() {
         return (
+            <Fragment>
+              <Header />
+               <main>
             <div>
-                <h1>Hello World</h1>
+             <h1>Hello World</h1>
             </div>
+            </main>
+            </Fragment>
         );
     }
 }
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import "../styles/header.css";
+
+class Header extends Component {
+    render  () {
+        return (
+            <header>Headerr</header>
+        )
+    }
+}
+
+export default Header;
