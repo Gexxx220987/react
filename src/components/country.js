68530d45048af1b9c2a789c9266a34449c87c486
German <gexxx@bk.ru>
2023-11-30 20:27:18 +0300

    new

--- src/components/country.js
+++ src/components/country.js
@@ -4,13 +4,22 @@ import Button from "react-bootstrap/Button";
 import "../styles/country.css";
 
 function Country(props) {
+
     const [selected, changeSelected] = useState(false);
+
    return( 
     <tr className = {selected ? "selected-country": ""}>
     <td>{props.country.name.common}</td>
      <td>{props.country.capital}</td>
-    <td><Button variant="success" onClick={() => changeSelected(true)}>ADD</Button></td></tr>
-    
+    <td>
+        {selected ? 
+        
+        <Button variant="danger" onClick={() => changeSelected(true)}>Remove</Button>:
+        <Button variant="success" onClick={() => changeSelected(true)}>ADD</Button> 
+        
+        }
+        </td>
+        </tr>
     );
 
 }
