402299b13b7dd7eb38428f90eb3f61d7c3cb51bf
German <gexxx@bk.ru>
2023-12-01 14:03:22 +0300

    new

--- src/components/country.js
+++ src/components/country.js
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 
-import "../styles/country.css";
 
 function Country(props) {
-
     const [selected, changeSelected] = useState(false);
 
-   return( 
-    <tr className = {selected ? "selected-country": ""}>
-    <td>{props.country.name.common}</td>
-     <td>{props.country.capital}</td>
-    <td>
-        {selected ? 
-        
-        <Button variant="danger" onClick={() => changeSelected(true)}>Remove</Button>:
-        <Button variant="success" onClick={() => changeSelected(true)}>ADD</Button> 
-        
-        }
-        </td>
+    return (
+        <tr className={selected ? 'selected-country' : ''}>
+            <td>{props.country.name.common}</td>
+            <td>{props.country.capital}</td>
+            <td>
+                {selected ? (
+                    <Button variant='danger' onClick={() => changeSelected(false)}>
+                        Remove
+                    </Button>
+                ) : (
+                    <Button variant='success' onClick={() => changeSelected(true)}>
+                        ADD
+                    </Button>
+                )}
+            </td>
         </tr>
     );
-
 }
 
 export default Country;
